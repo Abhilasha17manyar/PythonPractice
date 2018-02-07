4f2fbf5dda0c4541fa38352eb9f6765dd23fe808
Abhilasha <abhilasha.manyar@gmail.com>
2018-02-07 17:04:30 +0530

    First file commit in python class practice

---
+++ As1_ClassOfficeAddrs.py
@@ -0,0 +1,9 @@
+'''Write a Python class called officeAddress that has two attributes:
+DeskNumber and streetName.
+Make sure you have an init method that initializes the object appropriately.'''
+class officeAddress(object):
+    def __init__(self, DeskNumber, StreetName):
+        self.DeskNumber = DeskNumber
+        self.StreetName = StreetName
+    
+    
